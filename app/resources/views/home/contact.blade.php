88bf9cc20bfccf3c6e8a097705ff58f3ddd7ff3b
Adam Torok <torok-a@ulster.ac.uk>
2022-03-04 11:14:56 +0000

    example branch

--- app/resources/views/home/contact.blade.php
+++
@@ -1,7 +0,0 @@
-@extends('home.layout.app')
-@section('title', 'Contact Page')
-
-@section('content')
-Contact extends app
-@endsection
-
--- app/routes/web.php
+++ app/routes/web.php
@@ -3,22 +3,10 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    //return view('welcome');
-    return view('home.index');
+    return view('welcome');
 });
 
 Route::get('/info', function () {
     return phpinfo();
 });
 require __DIR__.'/auth.php';
-
-Route::get('/dashboard', function () {
-    return view('dashboard');
-})->middleware(['auth'])->name('dashboard');
-
-
-
-Route::get('/contact', function(){
-    return view('home.contact');
-});
-
